7f86c6b19543a5fe8db846741441427c493e41be
Robinn023 <114648209+Robinn023@users.noreply.github.com>
2024-03-22 12:12:44 +0100

    start van javascript

---
+++ js/kalender.js
@@ -0,0 +1,17 @@
+const datums = document.querySelector(".datums");
+const navs= document.querySelectorAll("#vorige, #volgende");
+
+const maanden = [
+    "janunari",
+    "februari",
+    "maart",
+    "april",
+    "mei",
+    "juni",
+    "juli",
+    "augustes",
+    "september",
+    "oktober",
+    "november",
+    "december",
+];
